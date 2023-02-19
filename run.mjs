4ea4e8da299f44d22e8f6e88ae5da7f8c6848702
Ray <Pecacheu@gmail.com>
2023-02-18 19:44:30 -0500

    v4.1.1
    Better COLORS!

--- run.mjs
+++ run.mjs
@@ -2,11 +2,11 @@
 import os from 'os'; import fs from 'fs'; import http from 'http';
 import https from 'https'; import {spawn} from 'child_process';
 import {dirname} from 'path'; import {fileURLToPath} from 'url';
-import conf from '../load.js';
-const VER="v4.1";
+import {C,msg,err} from './color.mjs'; import conf from '../load.js';
+const VER="v4.1.1";
 
 let ind,dir=dirname(dirname(fileURLToPath(import.meta.url)));
-const info={ips:getIPList(),dir:dir}, opt=conf.opts, msg=console.log;
+const info={ips:getIPList(),dir:dir}, opt=conf.opts;
 if(opt.autoInstOptional==null) opt.autoInstOptional=true;
 if(!opt.npm || !opt.npm.length) opt.npm=[''];
 if(verifyDepends()) conf.main(info); else runJSLoader();
@@ -14,7 +14,7 @@ if(verifyDepends()) conf.main(info); else runJSLoader();
 function verifyDepends() {
 	getOS(); let v=process.version;
 	if(!(Number(v.substr(1,v.indexOf('.')-1)) >= opt.minVer))
-		err(`Nodejs ${v} too old, requires >= v${opt.minVer}!`);
+		err(`Nodejs ${v} too old, requires >= v${opt.minVer}!`,1);
 	if(process.argv.length==3 && process.argv[2]=='.reload') {opt.deleteDir=1;return 0}
 	let p=1; for(let n=0,l=opt.npm.length,ns,name; n<l; ++n) {
 		ns=opt.npm[n].split(" as "), name=ns[0]; if(ns.length > 1) name=ns[1];
@@ -29,11 +29,11 @@ function verifyDepends() {
 }
 
 function runJSLoader() {
-	msg(`\x1b[2mNodejs AutoLoader ${VER} by Pecacheu\x1b[0m\n`+
+	msg(C.Di+`Node AutoLoader ${VER} by Pecacheu\n`+C.Rst+
 	`IP: ${info.ips} OS: ${info.os}, ${info.arch}\nCPU: ${info.cpu}*`+
 	info.cpus+(opt.debug?"Debug Mode Enabled.":''));
-	msg("\x1b[33mDependencies Missing!\x1b[0m\n"),checkNet(e => {
-		if(e) err("No Internet Connection!\n"+e);
+	msg(C.Ylo+"Dependencies Missing!\n"),checkNet(e => {
+		if(e) err("No Internet Connection!\n"+e,1);
 		if(opt.wgetFiles && opt.wgetFiles.length) {
 			msg("Fetching Resources..."); mkDir(dir+opt.wgetPath,1);
 			ind=0; get(opt.wgetFiles[ind]);
@@ -44,7 +44,7 @@ function get(uri) {(uri.startsWith('https:')?https:http).get(uri,httpRes)}
 function httpRes(rs) {
 	let st=rs.statusCode;
 	if(st>=301 && st<=308 && rs.headers.location) return get(rs.headers.location);
-	if(st!=200) err("Bad response code "+st);
+	if(st!=200) err("Bad response code "+st,1);
 	let fn=opt.wgetFiles[ind]; fn=fn.substr(fn.lastIndexOf('/')+1);
 	let file=fs.createWriteStream(dir+opt.wgetPath+fn); rs.pipe(file);
 	file.on('finish', () => {
@@ -60,14 +60,14 @@ function doInstall() {
 }
 function installRun() {
 	if(ind == opt.npm.length) remDir(dir+'/package-lock.json'),
-		msg("\x1b[32mInstall Complete!\x1b[0m"),process.exit();
+		msg(C.Grn+"Install Complete!"),process.exit();
 	let ns=opt.npm[ind].split(" as "),mod=ns[0],inst=mod; if(ns.length > 1) mod=ns[1];
 	++ind; if(opt.deleteDir || !fs.existsSync(dir+'/node_modules/'+mod)) {
-		msg("Installing",mod);
+		msg(C.BgMag+C.Whi+"Installing",mod);
 		let cmd=spawn('npm', ['i',inst], {cwd:dir, windowsHide:true, shell:true, stdio:'inherit'});
 		cmd.on('error', e => console.error(e));
-		cmd.on('close', c => c?err("Install failed!"):installRun());
-	} else msg("Skipping",mod),installRun();
+		cmd.on('close', c => c?err("Install failed!",1):installRun());
+	} else msg(C.Di+"Skipping",mod),installRun();
 }
 
 function mkDir(p,noDel) {
@@ -80,7 +80,6 @@ function remDir(p,c) {
 	let d=fs.readdirSync(p); for(let s in d) remDir(p+'/'+d[s],1); fs.rmdirSync(p);
 }
 
-function err(e) {console.error(`\x1b[31m${e}\x1b[0m`),process.exit(1)}
 function checkNet(cb) {https.get('https://google.com',()=>cb()).on('error',cb)}
 function getIPList() {
 	const ip=[], fl=os.networkInterfaces();
